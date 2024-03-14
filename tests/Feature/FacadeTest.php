2da694b1f8cb9f888fc012cafbcad7a3fd17550f
Adriyansyah Syaputra <adriansyahputra2626@gmail.com>
2024-03-14 16:58:24 +0700

    Facade, Routes, View

---
+++ tests/Feature/FacadeTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Facade;
+
+class FacadeTest extends TestCase
+{
+    public function testConfig()
+    {
+        $firstName1 = config('contoh.author.first');
+        $firstName2 = Config::get('contoh.author.first');
+
+        self::assertEquals($firstName1, $firstName2);
+    }
+}
