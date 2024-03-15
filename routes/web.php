719879c8b4e348547286172e88806ca446fdab05
Adriyansyah Syaputra <adriansyahputra2626@gmail.com>
2024-03-15 16:11:43 +0700

    Route Parameter & Named Route

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,28 @@ Route::redirect('/youtube', '/pzn');
 Route::fallback(function() {
     return "404 by Adriyansyah";
 });
+
+Route::get('/products/{id}', function($productId) {
+    return "Product $productId";
+})->name('product.detail');
+
+Route::get('/products/{product}/items/{item}', function($productId, $itemId) {
+    return "Product $productId, Item $itemId";
+})->name('product.item.detail');
+
+Route::get('/categories/{id}', function($categoryId) {
+    return "Category $categoryId";
+})->where('id', '[0-9]+')->name('category.detail');
+
+Route::get('/users/{id?}', function($userId = '404') {
+    return "User $userId";
+})->name('user.detail');
+
+Route::get('/product/{id}', function($id) {
+    $link = route('product.detail', ['id' => $id]);
+    return "Link $link";
+});
+
+Route::get('/product-redirect/{id}', function($id) {
+    return redirect()->route('product.detail', ['id' => $id]);
+});
--- tests/Feature/RoutingTest.php
+++ tests/Feature/RoutingTest.php
@@ -26,4 +26,37 @@ class RoutingTest extends TestCase
     $this->get('/tidakada')
     ->assertSeeText('404 by Adriyansyah');
   }
+
+  public function testRouteParameter()
+  {
+    $this->get('/products/1')
+    ->assertSeeText('Product 1');
+
+    $this->get('/products/1/items/XXX')
+    ->assertSeeText('Product 1, Item XXX');
+  }
+
+  public function testRouteparameterRegex()
+  {
+    $this->get('/categories/100')
+    ->assertSeeText("Category 100");
+  }
+
+  public function testRouteParameterOptional()
+  {
+    $this->get('/users/adriyansyah')
+    ->assertSeeText('User adriyansyah');
+
+    $this->get('/users/')
+    ->assertSeeText('User 404');
+  }
+
+  public function testNamed()
+  {
+    $this->get('/product/12345')
+    ->assertSeeText('Link http://localhost/products/12345');
+
+    $this->get('/product-redirect/12345')
+    ->assertSeeText('/products/12345');
+  }
 }
